8d31891115ba0e8143bbdf2a6d87f130ff403bb1
PANAKA-4696 <212013796+PANAKA-4696@users.noreply.github.com>
2025-09-01 00:06:21 +0900

    Discord-bot作成完了

---
+++ First_bot.py
@@ -0,0 +1,26 @@
+import discord
+
+#ボットトークンを指定(Discord Developer Portalから取得)
+TOKEN = "自分のBotTOKENをここに張り付ける。"
+
+#Discordクライアントを作成
+client = discord.Client(intents=discord.Intents.all())
+
+#ボットが起動したときに実行するイベントハンドラ
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+#メッセージを受け取ったときに実行するイベントハンドラ
+@client.event
+async def on_message(message):
+    #メッセージを送信したユーザーがボットでないことを確認
+    if message.author == client.user:
+        return
+    
+    #メッセージが"hello"だった場合、"hay"と返信
+    if message.content == "hello":
+        await message.channel.send("hay")
+
+#ボットを起動
+client.run(TOKEN)
